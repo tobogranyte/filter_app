b367e7f66b5b030be56e5160bf7514a50f727c2b
Tom Bolton <tom@mediabahn.com>
2013-04-11 10:35:12 -0400

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FilterApp::Application.config.secret_token = '1260a0c467cc8950eab0d71863c1891e88a08923a19487096f659073ac4bed955a2068d659ffd09fcabb667b581169e4556a74b59296238d7c3908c8433bbb35'
